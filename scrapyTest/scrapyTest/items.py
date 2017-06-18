91182102c0059f45c3e18cc592e6929cbfbd36d2
morganfly <786485758@qq.com>
2017-06-19 02:35:09 +0800

    1

--- scrapyTest/scrapyTest/items.py
+++ scrapyTest/scrapyTest/items.py
@@ -11,4 +11,7 @@ import scrapy
 class ScrapytestItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
+    type = scrapy.Field()
+    price = scrapy.Field()
+    car_name = scrapy.Field()
     pass
--- scrapyTest/scrapyTest/spiders/example.py
+++ scrapyTest/scrapyTest/spiders/example.py
@@ -2,26 +2,36 @@
 import scrapy
 from scrapyTest.items import ScrapytestItem
 from scrapy import Request
+import re
+
 
 # 少帅的spider
 class Car(scrapy.Spider):
     name = 'xcar'
     start_urls = ['http://newcar.xcar.com.cn/car/0-0-0-0-0-0-9-0-0-0-0-0/']
 
-    def start_requests(self):
-        for i in self.start_urls:
-            yield scrapy.Request(i, callback=self.carparese)
+    # def start_requests(self):
+    #     for i in self.start_urls:
+    #         yield scrapy.Request(i,callback=self.parse)
+    def parse(self, response):
 
-    def carparese(self, response):
         xpth = '//div[@class="cenl"]/h6/a/@href'
         link = response.xpath(xpth).extract()
 
         for i in link:
             url = 'http://newcar.xcar.com.cn' + i
-            print(url)
-            yield Request(url, callback=self.carparese2)
+            yield Request(url, callback=self.parse2)
+        # xpth='//p[@class="tw_gmpage"]/a[last()]/@href'
+        # next_url = response.xpath(xpth).extract()
+        net = response.body[response.body.find(b'tw_gmpage"><a'):response.body.find(b'id="tw_newcar')]
+        print(net)
+        url = re.findall(r'/car/0-0-0-0-0-0-9-0-0-0-0-\d/', net)
+        print(url)
+        for i in url:
+            yield Request('http://newcar.xcar.com.cn' + i, callback=self.parse)
 
-    def carparese2(self, response):
+    def parse2(self, response):
+
         item = ScrapytestItem()
         xpth = '//span[@class="lt_f1"]/text()|//div[@class="tt_h1"]/h1/text()'
         name2 = response.xpath(xpth).extract()
