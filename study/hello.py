c1399859a08211b7a9bc689180450689d512c9cd
youmm <youmm@tongdaohui.com>
2017-06-29 17:09:09 +0800

    阮千军

---
+++ study/hello.py
@@ -0,0 +1,22 @@
+name = input('please enter your name:')
+print("hello", name)
+
+
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 10000:
+            raise StopIteration()
+        return self.a
+
+
+for n in Fib():
+    print(n)
+
+
